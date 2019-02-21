8739a496ae706551f44a9f2186ce431877efa78d
Matthieu Therond <matthieu.therond@people-doc.com>
2019-02-21 16:40:11 +0100

    First modif for first PR

--- src/App.js
+++ src/App.js
@@ -9,16 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Je peux changer le contenu du site en live.
+            Hello William and Bertrand! first deploy on AWS! 
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
