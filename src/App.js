c4361ae4e9be3bca5ef56e4ad1abb83d002e8590
Matthieu Therond <matthieu.therond@people-doc.com>
2019-02-21 14:17:43 +0100

    added travis file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and not save to reload.
+            Je peux changer le contenu du site en live.
           </p>
           <a
             className="App-link"
